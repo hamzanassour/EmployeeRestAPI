ccbbb04c338648d95f887b98a3cdb05525b4b5eb
hamza <hamza.nassour13@gmail.com>
2023-03-09 00:58:59 +0100

    Employee Rest API

---
+++ src/main/java/com/example/employeerestapi/Repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.employeerestapi.Repository;
+
+import com.example.employeerestapi.Entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
+
